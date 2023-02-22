a1c0524b18af5d7f657bbe18044f0c9add15a957
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-22 14:23:46 -0800

    WIP. tabs. remove duplicated styles

--- src/components/Tabs/TabsTest.tsx
+++ src/components/Tabs/TabsTest.tsx
@@ -6,7 +6,7 @@ export function TabsTest() {
     }
     return (
         <div className="p-4">
-            <div className="p-4  min-h-[6rem] min-w-[18rem] max-w-4xl bg-cover bg-top border rounded-b-box rounded-tr-box flex items-center justify-center flex-wrap gap-2 overflow-x-hidden">
+            <div className="p-4  min-h-[6rem] min-w-[18rem] max-w-4xl bg-cover bg-top border rounded-b-2xl rounded-tr-2xl flex items-center justify-center flex-wrap gap-2 overflow-x-hidden">
                 <div className="tabs">
                     <a className="tab tab-lifted [--tab-color:#65b165]" onClick={onSelect}>Tab 1</a>
                     <a className="tab tab-lifted [--tab-color:#65b165] tab-active" onClick={onSelect}>Tab 2</a>
