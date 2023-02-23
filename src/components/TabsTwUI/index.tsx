aece35464c4daf7156d930b8f0398724e5f01b32
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-23 14:47:44 -0800

    WIP. tabs. update TabsTwUI layout

--- src/components/TabsTwUI/index.tsx
+++ src/components/TabsTwUI/index.tsx
@@ -54,18 +54,14 @@ function Tabs() {
                                 tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
                                 tabIdx === 0 ? 'rounded-l-lg' : '',
                                 tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
-                                'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10'
+                                'group relative min-w-0 flex-1 overflow-hidden py-4 px-4 focus:z-10',
+                                'text-sm font-medium text-center',
+                                'bg-white  hover:bg-gray-50',
                             )}
                             aria-current={tab.current ? 'page' : undefined}
                         >
                             <span>{tab.name}</span>
-                            <span
-                                aria-hidden="true"
-                                className={classNames(
-                                    tab.current ? 'bg-indigo-500' : 'bg-transparent',
-                                    'absolute inset-x-0 bottom-0 h-0.5'
-                                )}
-                            />
+                            <span className={classNames(tab.current ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-1')} aria-hidden="true"/>
                         </a>
                     ))}
                 </nav>
