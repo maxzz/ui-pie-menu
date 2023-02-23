427f17ed8961fd8e031c02ec5090f0587010f575
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-23 15:39:43 -0800

    WIP. tabs. update TabsTwUI layout. dark theme

--- src/components/TabsTwUI/index.tsx
+++ src/components/TabsTwUI/index.tsx
@@ -45,23 +45,23 @@ function Tabs() {
             </div>
 
             <div className="hidden sm:block">
-                <nav className="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
+                <nav className="isolate flex divide-x divide-violet-900 rounded-lg shadow" aria-label="Tabs">
                     {tabs.map((tab, tabIdx) => (
                         <a
                             key={tab.name}
                             href={tab.href}
                             className={classNames(
-                                tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+                                tab.current ? 'text-green-500' : 'text-green-500/50 hover:text-green-500',
                                 tabIdx === 0 ? 'rounded-l-lg' : '',
                                 tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
                                 'group relative min-w-0 flex-1 overflow-hidden py-4 px-4 focus:z-10',
                                 'text-sm font-medium text-center',
-                                'bg-white  hover:bg-gray-50',
+                                'bg-violet-900/20  hover:bg-violet-900/50',
                             )}
                             aria-current={tab.current ? 'page' : undefined}
                         >
                             <span>{tab.name}</span>
-                            <span className={classNames(tab.current ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-1')} aria-hidden="true"/>
+                            <span className={classNames(tab.current ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-1')} aria-hidden="true" />
                         </a>
                     ))}
                 </nav>
@@ -71,9 +71,9 @@ function Tabs() {
 }
 
 export function TabsTwUI() {
-    return(
+    return (
         <div className="m-3 p-4 bg-violet-600/40 border-violet-900/30 border rounded-md shadow">
             <Tabs />
         </div>
-    )
+    );
 }
