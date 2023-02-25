3fb4b4fedf0fa6fa7b7a5fed0ff2908319736380
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-25 01:13:46 -0800

    WIP. app. update menu demo

--- src/components/PieMenu/PieMenu.tsx
+++ src/components/PieMenu/PieMenu.tsx
@@ -4,24 +4,12 @@ export function PieMenu() {
             <input className='menu-toggler' id='menu-toggler' type='checkbox' />
             <label htmlFor='menu-toggler'></label>
             <ul>
-                <li className='menu-item'>
-                    <a className='fa fa-facebook' href='https://www.facebook.com/' target='_blank'></a>
-                </li>
-                <li className='menu-item'>
-                    <a className='fa fa-google' href='https://www.google.com/' target='_blank'></a>
-                </li>
-                <li className='menu-item'>
-                    <a className='fa fa-dribbble' href='https://dribbble.com/' target='_blank'></a>
-                </li>
-                <li className='menu-item'>
-                    <a className='fa fa-codepen' href='https://codepen.io/' target='_blank'></a>
-                </li>
-                <li className='menu-item'>
-                    <a className='fa fa-linkedin' href='https://www.linkedin.com/' target='_blank'></a>
-                </li>
-                <li className='menu-item'>
-                    <a className='fa fa-github' href='https://github.com/' target='_blank'></a>
-                </li>
+                <li className='menu-item'> <a target='_blank' className='fa fa-facebook' href='https://www.facebook.com'></a> </li>
+                <li className='menu-item'> <a target='_blank' className='fa fa-google' href='https://www.google.com'></a> </li>
+                <li className='menu-item'> <a target='_blank' className='fa fa-dribbble' href='https://dribbble.com'></a> </li>
+                <li className='menu-item'> <a target='_blank' className='fa fa-codepen' href='https://codepen.io'></a> </li>
+                <li className='menu-item'> <a target='_blank' className='fa fa-linkedin' href='https://www.linkedin.com'></a> </li>
+                <li className='menu-item'> <a target='_blank' className='fa fa-github' href='https://github.com'></a> </li>
             </ul>
         </nav>
     );
