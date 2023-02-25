18ee0a80309b28c7e055c4b75c6b439dd45ef70a
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-25 01:14:24 -0800

    WIP. app. update Section1_Tabs

--- src/components/Section1_Tabs/index.tsx
+++ src/components/Section1_Tabs/index.tsx
@@ -1,8 +1,5 @@
+import { classNames } from "../../utils";
 import { appState, useSnapshot } from "../store";
-
-function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
-}
 
 type Tab = { id: number; name: string; };
 
@@ -12,18 +9,21 @@ function TabButtons({ tabs, selected, onSelect }: { tabs: Tab[]; selected: numbe
             <button
                 key={tab.name}
                 className={classNames(
-                    idx === 0 ? 'rounded-l-lg' : '',
-                    idx === tabs.length - 1 ? 'rounded-r-lg' : '',
-                    tab.id === selected ? 'text-green-500' : 'text-green-500/50 hover:text-green-500',
-                    'group relative min-w-0 flex-1 overflow-hidden py-4 px-4 focus:z-10',
-                    'text-sm font-medium text-center transition-colors duration-300',
+                    idx === 0 ? 'rounded-l' : '',
+                    idx === tabs.length - 1 ? 'rounded-r' : '',
+                    tab.id === selected ? 'text-green-500 bg-violet-900/50' : 'text-green-500/70 hover:text-green-500',
+                    'group relative min-w-0 flex-1 overflow-hidden px-2 py-3 focus:z-10',
+                    'text-sm font-medium text-center select-none transition-colors duration-300',
                     'bg-violet-900/20  hover:bg-violet-900/50',
                 )}
                 aria-current={tab.id === selected ? 'page' : undefined}
                 onClick={() => onSelect?.(tab.id)}
             >
                 <span>{tab.name}</span>
-                <span className={classNames(tab.id === selected ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-1')} aria-hidden="true" />
+                <span
+                    className={classNames('absolute inset-x-0 bottom-0 h-0.5', tab.id === selected ? 'bg-indigo-500' : 'bg-transparent')}
+                    aria-hidden="true"
+                />
             </button>
         ))
         }
@@ -41,7 +41,7 @@ const tabs: Tab[] = [
 export function Section1_Tabs() {
     const snap = useSnapshot(appState);
     return (
-        <nav className="px-4 py-2 isolate flex divide-x divide-violet-900 shadow select-none" aria-label="Tabs">
+        <nav className="px-4 pt-4 pb-2 divide-violet-900 shadow isolate flex divide-x" aria-label="Tabs">
             <TabButtons tabs={tabs} selected={snap.activeTab} onSelect={(id) => {
                 appState.activeTab = id;
             }} />
