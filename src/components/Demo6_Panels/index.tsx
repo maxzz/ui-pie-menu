12ceed02ad60ed7509427e3a2f261a5ea20f1fc7
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-27 17:08:30 -0800

    WIP. Demo6_Panels. styles. 7. .detailTime

--- src/components/Demo6_Panels/index.tsx
+++ src/components/Demo6_Panels/index.tsx
@@ -47,31 +47,26 @@ function Cell_01({ index }: { index: number; }) {
     );
 }
 
+namespace Row0 {
+    export function Detail({ title1, title2, title3, ...rest }: HTMLAttributes<HTMLDivElement> & { title1: string; title2: string; title3: string; }) {
+        return (
+            <div className="pt-3 text-[9px] text-gray-400">
+                {title1}
+                <div className="text-[20px] font-bold text-gray-800">{title2}</div>
+                {title3}
+            </div>
+        );
+    }
+}
+
 function Cell_02({ index }: { index: number; }) {
     return (
         <div id="firstTop">
             <img style={allFlights[index].style} src={allFlights[index].src} />
             <div id="timecontainer">
-                <div id="detailDate">
-                    Bengaluru
-                    <div id="detailTime">6:20</div>
-                    June 12
-                </div>
-                <img
-                    style={{
-                        width: "30px",
-                        height: "26px",
-                        marginTop: "22px",
-                        marginLeft: "16px",
-                        marginRight: "16px"
-                    }}
-                    src="https://github.com/pizza3/asset/blob/master/airplane2.png?raw=true"
-                />
-                <div id="detailDate">
-                    New Delhi
-                    <div id="detailTime">8:45</div>
-                    June 12
-                </div>
+                <Row0.Detail title1="Bengaluru" title2="6:20" title3="June 12" />
+                <img className="w-[30px] h-[26px] mx-4 mt-6" src="https://github.com/pizza3/asset/blob/master/airplane2.png?raw=true"/>
+                <Row0.Detail title1="New Delhi" title2="8:45" title3="June 12" />
             </div>
         </div>
     );
@@ -86,9 +81,9 @@ namespace Row2 {
             </div>
         );
     }
-    export function FirstBehindRow({ children, ...rest }: HTMLAttributes<HTMLDivElement>) {
+    export function Column({ children, ...rest }: HTMLAttributes<HTMLDivElement>) {
         return (
-            <div className="mt-2 relative flex flex-col justify-between text-left" id="firstBehindRow">
+            <div className="mt-2 relative flex flex-col justify-between text-left">
                 {children}
             </div>
         );
@@ -97,21 +92,32 @@ namespace Row2 {
 
 function Cell_03({ index }: { index: number; }) {
     return (
-        <div id="firstBehindDisplay">
-            <Row2.FirstBehindRow>
+        <div id="firstBehind__Display">
+            <Row2.Column>
                 <Row2.Detail title="Flight Time">6:20 - 8:45</Row2.Detail>
                 <Row2.Detail title="Transfer">No</Row2.Detail>
-            </Row2.FirstBehindRow>
-            <Row2.FirstBehindRow>
+            </Row2.Column>
+            <Row2.Column>
                 <Row2.Detail title="Duration">2h 25 min</Row2.Detail>
                 <Row2.Detail title="Gate">8</Row2.Detail>
-            </Row2.FirstBehindRow>
-            <Row2.FirstBehindRow>
+            </Row2.Column>
+            <Row2.Column>
                 <Row2.Detail title="Boarding">5:35</Row2.Detail>
                 <Row2.Detail title="Seat">20A</Row2.Detail>
-            </Row2.FirstBehindRow>
+            </Row2.Column>
         </div>
     );
+}
+
+namespace Row3 {
+    export function Detail({ children, title, ...rest }: HTMLAttributes<HTMLDivElement>) {
+        return (
+            <div className="text-[1rem] font-bold">
+                {children}
+                <Utils.InfoLabel style={{ textAlign: 'left' }}>{title}</Utils.InfoLabel>
+            </div>
+        );
+    }
 }
 
 function Cell_04({ index, active }: { index: number; active: boolean; }) {
@@ -119,21 +125,15 @@ function Cell_04({ index, active }: { index: number; active: boolean; }) {
         <div id="second" style={{ transform: active ? `rotate3d(1, 0, 0, -180deg)` : `rotate3d(1, 0, 0, 0deg)`, transitionDelay: active ? "0.2s" : "0.2s" }}>
             <div id="secondTop" />
             <div id="secondBehind">
-                <div id="secondBehindDisplay">
-                    <div className="text-[1rem] font-bold">
-                        $100
-                        <Utils.InfoLabel style={{ textAlign: 'left' }}>Price</Utils.InfoLabel>
-                    </div>
-                    <div className="text-[1rem] font-bold">
-                        Economy
-                        <Utils.InfoLabel style={{ textAlign: 'left' }}>Class</Utils.InfoLabel>
-                    </div>
+                <div id="secondBehind__Display">
+                    <Row3.Detail title="Price">$100</Row3.Detail>
+                    <Row3.Detail title="Class">Economy</Row3.Detail>
                     <img className="w-24 h-8" src="https://github.com/pizza3/asset/blob/master/barcode.png?raw=true" />
                 </div>
 
                 <div id="third" style={{ transform: active ? `rotate3d(1, 0, 0, -180deg)` : `rotate3d(1, 0, 0, 0deg)`, transitionDelay: active ? "0.4s" : "0s" }}>
                     <div id="thirdTop" />
-                    <div id="secondBehindBottom">
+                    <div id="secondBehind__Bottom">
                         <button className="mt-2 px-32 py-1 text-xl font-bold rounded" style={{ color: allFlights[index].label, border: `1px solid ${allFlights[index].label}` }}>
                             Pay
                         </button>
