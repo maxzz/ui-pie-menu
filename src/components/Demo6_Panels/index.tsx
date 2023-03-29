c1c14812fbc0b884d73df51f6d4957b8f3285354
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-29 16:04:10 -0700

    WIP. Demo6_Panels. update card names

--- src/components/Demo6_Panels/index.tsx
+++ src/components/Demo6_Panels/index.tsx
@@ -1,9 +1,9 @@
-import { Children, HTMLAttributes, useState } from "react";
+import { HTMLAttributes, useState } from "react";
 import { allFlights } from "./data";
 import { IconBack, IconSettings } from "./UIIcons";
 import './styles.css';
 
-namespace Utils {
+namespace SharedUtils {
     export function AnimDots(props: HTMLAttributes<HTMLDivElement>) {
         return (
             <div className="absolute pl-0 left-4 top-2 w-5 h-2 overflow-hidden" {...props}>
@@ -23,32 +23,43 @@ namespace Utils {
     }
 }
 
-function Cell_01({ index }: { index: number; }) {
+function Card_01_Open({ index }: { index: number; }) {
     return (
         <div id="firstDisplay">
             <div className="text-[1.5rem] font-bold text-gray-800 flex-[0.5]">
-                <Utils.InfoLabel style={{ fontWeight: "bold", color: allFlights[index].label }}>From</Utils.InfoLabel>
-                BLR
-                <Utils.InfoLabel>Kempegowda International</Utils.InfoLabel>
+                <SharedUtils.InfoLabel style={{ fontWeight: "bold", color: allFlights[index].label }}>From</SharedUtils.InfoLabel>
+                SFO
+                <SharedUtils.InfoLabel>San Francisco International</SharedUtils.InfoLabel>
             </div>
 
             <div className="text-[1.5rem] font-bold text-gray-800 flex-[0.5]" style={{ marginTop: "15px" }}>
-                <Utils.AnimDots />
-                <Utils.AnimDots style={{ left: "32px" }} />
+                <SharedUtils.AnimDots />
+                <SharedUtils.AnimDots style={{ left: "32px" }} />
                 <img className="w-8" src="https://github.com/pizza3/asset/blob/master/airplane2.png?raw=true" />
             </div>
 
             <div className="text-[1.5rem] font-bold text-gray-800 flex-[0.5]">
-                <Utils.InfoLabel style={{ fontWeight: "bold", color: allFlights[index].label }}>To</Utils.InfoLabel>
+                <SharedUtils.InfoLabel style={{ fontWeight: "bold", color: allFlights[index].label }}>To</SharedUtils.InfoLabel>
                 DEL
-                <Utils.InfoLabel>Indira Gandhi International</Utils.InfoLabel>
+                <SharedUtils.InfoLabel>Indira Gandhi International</SharedUtils.InfoLabel>
             </div>
         </div>
     );
 }
 
-namespace Row0 {
-    export function Detail({ title1, title2, title3, ...rest }: HTMLAttributes<HTMLDivElement> & { title1: string; title2: string; title3: string; }) {
+function Card_01_closed({ index }: { index: number; }) {
+    return (
+        <div id="firstTop">
+            <img src={allFlights[index].src} style={allFlights[index].style} />
+            <div className="pr-3 pt-3 flex">
+                <Detail title1="San Francisco" title2="6:20" title3="June 12" />
+                <img className="w-[30px] h-[26px] mx-4 mt-6" src="https://github.com/pizza3/asset/blob/master/airplane2.png?raw=true" />
+                <Detail title1="New Delhi" title2="8:45" title3="June 12" />
+            </div>
+        </div>
+    );
+
+    function Detail({ title1, title2, title3, ...rest }: HTMLAttributes<HTMLDivElement> & { title1: string; title2: string; title3: string; }) {
         return (
             <div className="pt-3 text-[9px] text-gray-400">
                 {title1}
@@ -59,29 +70,33 @@ namespace Row0 {
     }
 }
 
-function Cell_02({ index }: { index: number; }) {
+function Card_02_open({ index }: { index: number; }) {
     return (
-        <div id="firstTop">
-            <img src={allFlights[index].src} style={allFlights[index].style} />
-            <div className="pr-3 pt-3 flex">
-                <Row0.Detail title1="Bengaluru" title2="6:20" title3="June 12" />
-                <img className="w-[30px] h-[26px] mx-4 mt-6" src="https://github.com/pizza3/asset/blob/master/airplane2.png?raw=true" />
-                <Row0.Detail title1="New Delhi" title2="8:45" title3="June 12" />
-            </div>
+        <div id="firstBehind__Display">
+            <Column>
+                <Detail title="Flight Time">6:20 - 8:45</Detail>
+                <Detail title="Transfer">No</Detail>
+            </Column>
+            <Column>
+                <Detail title="Duration">2h 25 min</Detail>
+                <Detail title="Gate">8</Detail>
+            </Column>
+            <Column>
+                <Detail title="Boarding">5:35</Detail>
+                <Detail title="Seat">20A</Detail>
+            </Column>
         </div>
     );
-}
 
-namespace Row2 {
-    export function Detail({ children, title, ...rest }: HTMLAttributes<HTMLDivElement>) {
+    function Detail({ children, title, ...rest }: HTMLAttributes<HTMLDivElement>) {
         return (
             <div className="text-[1rem] font-bold">
                 {children}
-                <Utils.InfoLabel>{title}</Utils.InfoLabel>
+                <SharedUtils.InfoLabel>{title}</SharedUtils.InfoLabel>
             </div>
         );
     }
-    export function Column({ children, ...rest }: HTMLAttributes<HTMLDivElement>) {
+    function Column({ children, ...rest }: HTMLAttributes<HTMLDivElement>) {
         return (
             <div className="mt-2 relative flex flex-col justify-between text-left">
                 {children}
@@ -90,38 +105,37 @@ namespace Row2 {
     }
 }
 
-function Cell_03({ index }: { index: number; }) {
+function Card_03_open({ index, active }: { index: number; active: boolean; }) {
     return (
-        <div id="firstBehind__Display">
-            <Row2.Column>
-                <Row2.Detail title="Flight Time">6:20 - 8:45</Row2.Detail>
-                <Row2.Detail title="Transfer">No</Row2.Detail>
-            </Row2.Column>
-            <Row2.Column>
-                <Row2.Detail title="Duration">2h 25 min</Row2.Detail>
-                <Row2.Detail title="Gate">8</Row2.Detail>
-            </Row2.Column>
-            <Row2.Column>
-                <Row2.Detail title="Boarding">5:35</Row2.Detail>
-                <Row2.Detail title="Seat">20A</Row2.Detail>
-            </Row2.Column>
+        <div id="second" style={{ transform: active ? `rotate3d(1, 0, 0, -180deg)` : `rotate3d(1, 0, 0, 0deg)`, transitionDelay: active ? "0.2s" : "0.2s" }}>
+            <div id="secondTop" />
+            <div id="secondBehind">
+                <div id="secondBehind__Display">
+                    <Row3_Detail title="Price">$100</Row3_Detail>
+                    <Row3_Detail title="Class">Economy</Row3_Detail>
+                    <img className="w-24 h-8" src="https://github.com/pizza3/asset/blob/master/barcode.png?raw=true" />
+                </div>
+
+                <div id="third" style={{ transform: active ? `rotate3d(1, 0, 0, -180deg)` : `rotate3d(1, 0, 0, 0deg)`, transitionDelay: active ? "0.4s" : "0s" }}>
+                    <div id="thirdTop" />
+                    <div id="secondBehind__Bottom">
+                        <Card_04_open index={index} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
-}
 
-namespace Row3 {
-    export function Detail({ children, title, ...rest }: HTMLAttributes<HTMLDivElement>) {
+    function Row3_Detail({ children, title, ...rest }: HTMLAttributes<HTMLDivElement>) {
         return (
             <div className="text-[1rem] font-bold">
                 {children}
-                <Utils.InfoLabel style={{ textAlign: 'left' }}>{title}</Utils.InfoLabel>
+                <SharedUtils.InfoLabel style={{ textAlign: 'left' }}>{title}</SharedUtils.InfoLabel>
             </div>
         );
     }
-}
 
-namespace Row4 {
-    export function Detail({ index, ...rest }: HTMLAttributes<HTMLDivElement> & { index: number; }) {
+    function Card_04_open({ index, ...rest }: HTMLAttributes<HTMLDivElement> & { index: number; }) {
         return (
             <button className="mt-2 px-32 py-1 text-xl font-bold rounded" style={{ color: allFlights[index].label, border: `1px solid ${allFlights[index].label}` }}>
                 Pay
@@ -130,40 +144,18 @@ namespace Row4 {
     }
 }
 
-function Cell_04({ index, active }: { index: number; active: boolean; }) {
-    return (
-        <div id="second" style={{ transform: active ? `rotate3d(1, 0, 0, -180deg)` : `rotate3d(1, 0, 0, 0deg)`, transitionDelay: active ? "0.2s" : "0.2s" }}>
-            <div id="secondTop" />
-            <div id="secondBehind">
-                <div id="secondBehind__Display">
-                    <Row3.Detail title="Price">$100</Row3.Detail>
-                    <Row3.Detail title="Class">Economy</Row3.Detail>
-                    <img className="w-24 h-8" src="https://github.com/pizza3/asset/blob/master/barcode.png?raw=true" />
-                </div>
-
-                <div id="third" style={{ transform: active ? `rotate3d(1, 0, 0, -180deg)` : `rotate3d(1, 0, 0, 0deg)`, transitionDelay: active ? "0.4s" : "0s" }}>
-                    <div id="thirdTop" />
-                    <div id="secondBehind__Bottom">
-                        <Row4.Detail index={index} />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-function Cell({ index }: { index: number; }) {
+function AllCards({ index }: { index: number; }) {
     const [active, handleActive] = useState(false);
     return (
         <div id="cardContainer" style={{ height: active ? `300px` : `100px`, transition: "0.9s" }} onClick={() => { handleActive(!active); }}>
-            <Cell_01 index={index} />
+            <Card_01_Open index={index} />
 
             <div id="first" style={{ transform: active ? `rotate3d(1, 0, 0, -180deg)` : `rotate3d(1, 0, 0, 0deg)`, transitionDelay: active ? "0s" : "0.3s" }}>
-                <Cell_02 index={index} />
+                <Card_01_closed index={index} />
 
                 <div id="firstBehind">
-                    <Cell_03 index={index} />
-                    <Cell_04 index={index} active={active} />
+                    <Card_02_open index={index} />
+                    <Card_03_open index={index} active={active} />
                 </div>
             </div>
         </div >
@@ -185,7 +177,7 @@ const Header = (
             <div className="my-1 text-[10px] flex flex-col text-left tracking-widest">
                 Your Trip
                 <div className="text-[28px] leading-[28px] font-bold tracking-wide flex items-center">
-                    BLR - DEL
+                    SFO - DEL
                     <div className="relative ml-2 px-1.5 py-1 text-[10px] leading-[10px] font-thin text-white bg-blue-500 border-blue-600 border rounded">
                         One Way
                     </div>
@@ -208,7 +200,7 @@ export function Demo6_Panels() {
                 <div className="demo6-panels">
                     {Header}
                     {arr.map((val, idx) => (
-                        <Cell index={idx} key={idx} />
+                        <AllCards index={idx} key={idx} />
                     ))}
                 </div>
             </div>
